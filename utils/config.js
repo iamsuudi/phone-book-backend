2975aa11d928edfa909175c1d86789b05a39718e
suudi <suudiabdulfetah@gmail.com>
2024-04-09 15:32:22 +0300

    feat: Create Person model and upload to atlas

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,7 @@
 const port = 3001;
+const mongoURL = `mongodb+srv://abdulfetah:${process.argv[2]}@suudi.nro6rmy.mongodb.net/phonebook?retryWrites=true&w=majority&appName=suudi`;
 
 module.exports = {
     port,
+    mongoURL,
 }
