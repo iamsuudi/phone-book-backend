7ceba5f76e81a8290a8ffde4c2a39bbd3baac824
suudi <suudiabdulfetah@gmail.com>
2024-04-10 14:12:40 +0300

    Add minLength validation on name

--- models/Person.js
+++ models/Person.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const personSchema = mongoose.Schema({
     name: {
         type: String,
+        minLength: 3,
         required: true,
     },
     number: {
